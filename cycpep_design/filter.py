6c3a2ffe0764ae4fae020371f9773a6ec43669c3
ks <ks2297@njit.edu>
2026-04-03 15:49:44 -0400

    Update filter.py

--- cycpep_design/filter.py
+++ cycpep_design/filter.py
@@ -10,28 +10,28 @@ def filter(csv_file, to_rosetta):
     # read csv
     df = pd.read_csv(file_path)
 
-    # 筛选符合条件的数据
+    # Filter data that meets the criteria
     # filtered_df = df[(df['plddt'] > 80) & (df['iptm'] > 0.5) & (df['ipae'] < 0.35) & (df['rmsd'] < 3.5)]
 
-    # 保存筛选后的数据到新 CSV 文件
+    # Save filtered data to a new CSV file
     # filtered_df.to_csv('filtered_data.csv', index=False)
 
-    # 筛选符合条件的数据
+    # Filter data that meets the criteria
     filtered_df = df[(df['plddt'] > 80) & (df['iptm'] > 0.5) & (df['ipae'] < 0.35) & (df['rmsd'] < 3.5)]
 
-    # 遍历筛选出的行并移动文件
+    # Iterate through filtered rows and copy files
     for _, row in filtered_df.iterrows():
-        # 构建文件的完整路径
+        # Build the full file path
         src_path = os.path.join(
             row['folder'], f"{row['protein_id']}_{row['backbone']}_{row['seqid']}_unrelaxed_{row['file']}.pdb"
         )
         
         if os.path.exists(src_path):
-            # 将文件移动到目标文件夹
+            # Copy the file to the destination folder
             dest_path = os.path.join(output_path, os.path.basename(src_path))
             shutil.copy(src_path, dest_path)
-            # print(f"已移动文件: {src_path} 到 {dest_path}")
+            # print(f"Copied file: {src_path} to {dest_path}")
         else:
-            print(f"文件不存在，无法复制: {src_path}")
+            print(f"File does not exist, cannot copy: {src_path}")
 
     return "done"
