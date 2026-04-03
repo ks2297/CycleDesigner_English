3fe8d74bf04e0c8e78dc111de900155fc6ba83c4
ks <ks2297@njit.edu>
2026-04-03 15:51:26 -0400

    Update fix_pdb.py

--- cycpep_design/fix_pdb.py
+++ cycpep_design/fix_pdb.py
@@ -3,11 +3,11 @@ from pdbfixer import PDBFixer
 from openmm.app import PDBFile
 
 def fix_missing_residues_and_gaps(input_pdb, output_pdb):
-    # 加载PDB文件
+    # Load the PDB file
     fixer = PDBFixer(filename=input_pdb)
 
-    # 查找缺失的残基并补齐
-    # print("查找并修复缺失的氨基酸残基以及不连续的序列...")
+    # Find and fill in missing residues
+    # print("Finding and fixing missing amino acid residues and sequence gaps...")
     fixer.findMissingResidues()
     
     if fixer.missingResidues:
@@ -15,26 +15,26 @@ def fix_missing_residues_and_gaps(input_pdb, output_pdb):
     else:
         print("No missing residues found.")
 
-    # 补齐所有缺失残基
+    # Find and add all missing atoms
     fixer.findMissingAtoms()
     fixer.addMissingAtoms()
 
-    # 处理不连续的残基（gaps），如果存在，需要填补这些间隙
+    # Handle nonstandard residues (gaps); replace them if present
     fixer.findNonstandardResidues()
     fixer.replaceNonstandardResidues()
 
-    # 添加缺失的氢原子
-    # print("添加缺失的氢原子...")
+    # Add missing hydrogen atoms
+    # print("Adding missing hydrogen atoms...")
     fixer.addMissingHydrogens()
 
-    # 保存修复后的PDB文件
+    # Save the repaired PDB file
     print(f"fixed pdb saved to{output_pdb}")
     with open(output_pdb, 'w') as out_file:
         PDBFile.writeFile(fixer.topology, fixer.positions, out_file)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        # print("用法: python fix_pdb.py input_pdb output_pdb")
+        # print("Usage: python fix_pdb.py input_pdb output_pdb")
         sys.exit(1)
 
     input_pdb = sys.argv[1]
